ed79f76a8aa6200c7784aabdb94304628ac5e7ea
Jefriline <Jefferlop2006@gmail.com>
2025-03-29 14:57:51 -0500

    Updated

--- src/Components/CreateTask/CreateTask.jsx
+++ src/Components/CreateTask/CreateTask.jsx
@@ -1,20 +1,57 @@
-import React from 'react'
+import { useState } from 'react'
 import './CreateTask.css'
 
 export const CreateTask = () => {
-    return (
-      <div className="container">
-        <h2>To-Do List</h2>
-        <input type="text" placeholder="Title" />
-        <textarea placeholder="Description"></textarea>
-        <button className="add-task">Add Task</button>
-        <div className="filter-buttons">
-          <button className="filter-btn">All</button>
-          <button className="filter-btn">Pending</button>
-          <button className="filter-btn">Completed</button>
-        </div>
-      </div>
-    );
-};
 
-  
+  const [task, setTask] = useState({
+    tittle: '',
+    description: ''
+  });
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (event) => {
+    event.preventDefault();
+    const currentTask = {
+      id: Date.now(),
+      tittle: task.tittle,
+      description: task.description,
+      completed: false,
+    }
+    setTasks([...tasks, currentTask]);
+    setTask({
+      tittle: '',
+      description: ''
+    });
+  }
+
+
+
+  return (
+    <div className="container">
+      <h2>To-Do List</h2>
+      <form action="" onSubmit={addTask}>
+      <input type="text" onChange={(e) => setTask({ ...task, tittle: e.target.value })} placeholder="Title" />
+      <textarea placeholder="Description" onChange={(e) => setTask({ ...task, description: e.target.value })}></textarea>
+      <button className="add-task">Add Task</button>
+      </form>
+      <div className="filter-buttons">
+        <button className="filter-btn">All</button>
+        <button className="filter-btn">Pending</button>
+        <button className="filter-btn">Completed</button>
+      </div>
+      <ul>
+        {
+          tasks.map((task) => (
+              <li key={task.id}>
+                <div>
+                  <h3>{task.tittle}</h3>
+                  <p>{task.description}</p>
+                </div>
+                <button>Delete</button>
+              </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
